4a731908711f9262bd92e06eab222ca1c154cf48
driuft <air.socarras@gmail.com>
2022-10-19 21:39:56 -0400

    Init commit

---
+++ app/src/main/java/com/driuft/androidhunt/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.driuft.androidhunt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * [T]
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
